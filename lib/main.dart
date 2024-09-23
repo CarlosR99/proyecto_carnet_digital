7dee3d4a11d851cf4f81665eff1a2e52ebc20d0d
CarlosR99 <jose.carlos.silva@correounivalle.edu.co>
2024-09-22 20:18:25 -0500

    feat: Agregar soporte para autenticación biométrica
    Agrega soporte para autenticación biométrica utilizando huella digital y Face ID en dispositivos Android e iOS.
    
    - Agrega los permisos necesarios en el archivo AndroidManifest.xml y Info.plist.
    - Importa los plugins necesarios en los archivos generated_plugins.cmake, generated_plugin_registrant.cc y GeneratedPluginRegistrant.swift.
    - Crea una nueva clase FingerprintService en el archivo finger_print.dart para manejar la autenticación biométrica.
    - Agrega la funcionalidad de autenticación biométrica en las pantallas de inicio de sesión y recuperación de contraseña.
    - Actualiza las importaciones de los archivos de las pantallas para reflejar los cambios en la estructura de carpetas.
    
    Refs #4

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:proyecto_carnet_digital/screens/login_page_screen.dart';
-import 'package:proyecto_carnet_digital/themes/app_theme.dart';
-import 'package:proyecto_carnet_digital/screens/recuperar_contra_screen.dart';
-import 'package:proyecto_carnet_digital/theme/theme.dart';
+import 'package:proyecto_carnet_digital/src/util/screens.dart';
 
 void main() {
-  runApp(const RecuperarContrasenaApp());
+  runApp(const MyApp());
 }
 
-class RecuperarContrasenaApp extends StatelessWidget {
-  const RecuperarContrasenaApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme, // Aplica el tema global
-      home: const LoginPage(),
+      //theme: AppTheme.lightTheme, // Aplica el tema global
+      initialRoute: '/Home',
+      routes: {
+        '/Home': (_) => const WelcomeScreen(),
+        '/login': (_) => const LoginPage(),
+        '/recuperar': (_) => const RecuperarContrasenaScreen(),
+      },
     );
   }
 }
